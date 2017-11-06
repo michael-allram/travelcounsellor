4b9bddc01a99531f40eaec8da95644d92daaecf6
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-11-06 15:14:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-//insert your functions here
+function street_to_geo(street){
+      //encodes the street string into url friendly format
+      var street_encoded = encodeURIComponent(street);
+      var uri = "http://nominatim.openstreetmap.org/search/" + street_encoded + "?format=json";
+      alert(uri);
+      //makes the the jquery call and gets json back 
+      $.getJSON(uri, function(data) {
+        //make js obj containing lat and long
+        var obj = {latitude: data[0].lat, longitude: data[0].lon};
+        alert(obj.latitude + " " + obj.longitude);
+        //return obj; 
+      });
+      
+    }
+
 
