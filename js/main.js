9b48986a4af5ce293e1c598086b5991733d61865
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-11-06 15:20:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+
+// Wait for user finishing typing
+// callback: The callback function
+// wait: The number of milliseconds to wait after the the last key press before firing the callback
+// highlight: Highlights the element when it receives focus
+// allowSubmit: Allows a non-multiline element to be submitted (enter key) regardless of captureLength
+// captureLength: Minimum # of characters necessary to fire the callback
+
+var options = {
+    callback: function (value) { alert("street entered");},
+    wait: 750,
+    highlight: true,
+    allowSubmit: false,
+    captureLength: 2
+}
+
+$("#street").typeWatch( options );
+
+
+
 function street_to_geo(street){
       //encodes the street string into url friendly format
       var street_encoded = encodeURIComponent(street);
