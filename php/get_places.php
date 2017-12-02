22c0fd27dcc12bba65c8eedd4f39d02708a9e2eb
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-02 17:41:50 +0100

    Update get_places.php

--- php/get_places.php
+++ php/get_places.php
@@ -3,12 +3,13 @@ $latitude = $_GET['lat'];
 $longitude = $_GET['long'];
 $radius = $_GET['radius'];
 $type = $_GET['type'];
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='. $latitude . ','  . $longitude .'&radius=' . $radius . '&type=' . $type . '&key=AIzaSyBBxg4WQz_rdeQT8_0b8rX9Y_7CEiBOB4E');
-$result = curl_exec($ch);
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='. $latitude . ','  . $longitude .'&radius=' . $radius . '&type=' . $type . '&key=AIzaSyBBxg4WQz_rdeQT8_0b8rX9Y_7CEiBOB4E');
+//$result = curl_exec($ch);
+$json=file_get_contents('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='. $latitude . ','  . $longitude .'&radius=' . $radius . '&type=' . $type . '&key=AIzaSyBBxg4WQz_rdeQT8_0b8rX9Y_7CEiBOB4E');
 
-$data =  json_decode($result);
-//echo $data.results.rating;
+$data =  json_decode($json);
+echo $data->results->rating;
 
 
 ?>
