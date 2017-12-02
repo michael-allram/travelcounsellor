af7901c1cd64df8c1d095f4176608a1f28c56e0f
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-02 17:18:11 +0100

    Update get_places.php

--- php/get_places.php
+++ php/get_places.php
@@ -1,5 +1,12 @@
+<?php
+$latitude = "10.0";
+$longitude = "20.0";
+$radius = 
+$type = 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/place/search/json?location='. $latitude . ','  . $longitude .'&radius=500&name=test&sensor=false&key=(api key)');
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='. $latitude . ','  . $longitude .'&radius=500&key=AIzaSyBBxg4WQz_rdeQT8_0b8rX9Y_7CEiBOB4E');
 $result = curl_exec($ch);
 echo $result;
+
+
+?>
