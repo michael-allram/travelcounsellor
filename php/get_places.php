ee182909f5e6cfb683fdc281eb4c5cf6672c62d3
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-03 13:31:54 +0100

    Update get_places.php

--- php/get_places.php
+++ php/get_places.php
@@ -32,7 +32,7 @@ $data =  json_decode($json);
 //make table with content
 echo "<table>";
 for($i=0;$i<20;$i++){
-  echo "<tr>";
+  echo "<tr onClick="showDetails('$data->results[$i]->place_id')">";
   echo "<td>" . $data->results[$i]->name . "</td>";
   echo "<td>" . $data->results[$i]->vicinity . "</td>";
   echo "<td>" . $data->results[$i]->open_now . "</td>";
