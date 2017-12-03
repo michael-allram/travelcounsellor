0d92eb14616c4a4cc07651bf1a56d81ea9b978cf
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-03 14:06:02 +0100

    Create get_details.php

---
+++ php/get_details.php
@@ -0,0 +1,13 @@
+<?php
+$place_id = $_GET['place_id'];
+
+$json=file_get_contents('https://maps.googleapis.com/maps/api/place/details/json?placeid=' . $place_id . '&key=AIzaSyBBxg4WQz_rdeQT8_0b8rX9Y_7CEiBOB4E');
+//decode json
+$data =  json_decode($json);
+
+echo $data->results[0]->photos[0]->photo_reference;
+
+
+
+
+?>
