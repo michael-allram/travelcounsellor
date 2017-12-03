635e88b6df5b188e2d12e45e3551b31eff2c27e6
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-03 13:35:23 +0100

    Update get_places.php

--- php/get_places.php
+++ php/get_places.php
@@ -32,12 +32,13 @@ $data =  json_decode($json);
 //make table with content
 echo "<table>";
 for($i=0;$i<20;$i++){
-  $placeid = $data->results[$i]->placeid;
-  echo "<tr onClick=\"showDetails('$place_id')\">";
+  $placeid = $data->results[$i]->place_id;
+  echo "<tr onClick=\"showDetails('$placeid')\">";
   echo "<td>" . $data->results[$i]->name . "</td>";
   echo "<td>" . $data->results[$i]->vicinity . "</td>";
   echo "<td>" . $data->results[$i]->open_now . "</td>";
   echo "<td>" . $data->results[$i]->rating . "</td>";
+  echo "<td>" . $data->results[$i]->place_id . "</td>";
   echo "<td>" . round(distance($latitude,$longitude,$data->results[$i]->geometry->location->lat,$data->results[$i]->geometry->location->lng,"K")*1000) . "</td>";
   echo "</tr>";
 }
