db52d558ef40b9f3143a6cbc847c8c735e7789bc
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-04 15:59:22 +0100

    Update get_places.php

--- php/get_places.php
+++ php/get_places.php
@@ -36,7 +36,7 @@ echo "<tr><td>Name</td><td>Street</td><td>Rating</td><td>Distance</td><td>is ope
  
 for($i=0;$i<20;$i++){
   $placeid = $data->results[$i]->place_id;
-  if($placeid == "") {echo "<td>No Place Found</td>"; continue;}
+  if($placeid == "") {echo "<td>No Place Found</td>"; exit;}
   echo "<tr onClick=\"showDetails('$placeid')\">";
   echo "<td>" . $data->results[$i]->name . "</td>";
   echo "<td>" . $data->results[$i]->vicinity . "</td>";
