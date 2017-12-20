b39d6b33e5a75779135ed34700760ee51e9bf796
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-20 17:48:25 +0100

    Create db_connect.php

---
+++ php/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "mysql.itas.cc";
+$username = "admin_tc";
+$password = "Cdq1m6@6";
+$database = "admin_travelcounsellor";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+
+
+
+?>
