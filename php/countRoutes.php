4dbf732f73cf594d76fde850905ac1eadb1791e8
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2017-12-20 20:17:45 +0100

    Create countRoutes.php

---
+++ php/countRoutes.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'db_connect.php';
+$cookie_id = $_COOKIE['travelcounsellorid'];
+$sql = "SELECT COUNT(*) as COUNTED FROM my_route WHERE cookie_id = $cookie_id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo $row["COUNTED"];
+} else {
+    echo "0";
+}
+$conn->close();
+
+
+?>
