ffa17681ce4364c5c6f1ebf61c6d4c392d024762
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2018-01-13 16:42:18 +0100

    Update showMyRoute.php

--- php/showMyRoute.php
+++ php/showMyRoute.php
@@ -15,12 +15,12 @@ $sql = "SELECT * FROM my_route";
 echo $sql;
 
 if ($result = $mysqli->query($sql)) {
-
+    echo "<table>";
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
         printf ("<tr><td>%s</td><td>%s</td><td>%f</td><td><a href='javascript:delete(id)'>%s</a></td></tr>", $row["name"], $row["street"], $row["rating"], $row["id"]);
     }
-
+    echo "</table>";
     /* free result set */
     $result->free();
 }
