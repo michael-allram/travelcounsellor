aac4e9735d0f295b3825a71a76c4685f8814b4eb
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2018-01-14 14:22:57 +0100

    Update showMyRoute.php

--- php/showMyRoute.php
+++ php/showMyRoute.php
@@ -20,13 +20,27 @@ if ($result = $conn->query($sql)) {
       return 0; 
     }
     $output .= "<div id='myRouteHeadline'>My Route</div>";
-    $output .= "<table>";
+    $output .= "<div class='datatable'>";
+      $output .= "<div class='table-header'>";
+        $output .= "<div class='name'>Name</div>";
+        $output .= "<div class='street'>Street</div>";
+        $output .= "<div class='rating'>Rating</div>";
+      $output .= "</div>";
+      $output .= "<div class='row'>";
+        $output .= "<div class='data'>";
+          $output .= "<div class='name'>" . $row['name'] . "</div>";
+          $output .= "<div class='street'>" . $row['street'] . "</div>";
+          $output .= "<div class='rating'>" . $row['rating'] . "</div>";
+          $output .= "<div class='del' onClick='delMyRoute(" . $row['id'] . ")>del</div>";
+        
+        
+    //$output .= "<table>";
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
-       $output .= "<tr class='row'><td>" . $row['name'] . "</td><td>" . $row['street']  . "</td><td>" . $row['rating']  . "</td><td onClick='delMyRoute(" . $row['id'] . ")'>DEL</td></tr>";
+       //$output .= "<tr class='row'><td>" . $row['name'] . "</td><td>" . $row['street']  . "</td><td>" . $row['rating']  . "</td><td onClick='delMyRoute(" . $row['id'] . ")'>DEL</td></tr>";
      
     }
-    $output .= "</table>";
+    //$output .= "</table>";
     /* free result set */
     $result->free();
 }
