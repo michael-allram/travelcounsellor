ce5da76d2004e67b4d3668da6a8ea81babb87c95
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2018-01-14 22:34:13 +0100

    Create navigation.php

---
+++ php/navigation.php
@@ -0,0 +1,44 @@
+<?php
+if(!isset($_COOKIE['travelcounsellorid'])){
+	echo "no entry because of no cookie";	
+	return 0;
+}
+	      
+	$cookieId = $_COOKIE['travelcounsellorid'];
+  
+	include 'db_connect.php';
+
+
+	$sql = "SELECT * FROM my_route WHERE cookie_id = '$cookieId'";
+		
+
+	if ($result = $conn->query($sql)) {
+  
+    		$row_cnt = $result->num_rows;
+    		if($row_cnt < 1) {
+      			echo "no results";
+      			return 1; 
+    		}
+        
+	    $output .= "www.google.at/maps/dir/";
+	 while ($row = $result->fetch_assoc()) {
+      $street = $row['street']; 
+		  $output .= str_replace(" ", "+", $street);
+      $output .= "/";
+			 
+	 }
+			
+	}
+	$result->free();
+	$conn->close();
+  
+  echo "<a href='" . $output . "'></a>";
+	
+
+
+?>
+
+	      
+  					
+			
+  		
