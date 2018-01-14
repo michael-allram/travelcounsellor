6ead14f784fda8cea5d101e21114e38b36f2b722
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2018-01-14 20:29:11 +0100

    Update maps.php

--- php/maps.php
+++ php/maps.php
@@ -115,7 +115,7 @@ if(!isset($_COOKIE['travelcounsellorid'])){
     		}
 	
 	 while ($row = $result->fetch_assoc()) {
-		echo "<li value='test'>" . $row['street'] . "</li>";
+		echo "<li>" . $row['street'] . " (" . $row['name'] . ")" . "</li>";
 			 
 	 }
 			
