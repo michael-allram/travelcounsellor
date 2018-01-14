b09cd645e97c9ef3f454068b71a3860427d7b616
Michael Allram <33128303+michael-allram@users.noreply.github.com>
2018-01-14 14:24:13 +0100

    Update showMyRoute.php

--- php/showMyRoute.php
+++ php/showMyRoute.php
@@ -28,19 +28,21 @@ if ($result = $conn->query($sql)) {
       $output .= "</div>";
       $output .= "<div class='row'>";
         $output .= "<div class='data'>";
-          $output .= "<div class='name'>" . $row['name'] . "</div>";
-          $output .= "<div class='street'>" . $row['street'] . "</div>";
-          $output .= "<div class='rating'>" . $row['rating'] . "</div>";
-          $output .= "<div class='del' onClick='delMyRoute(" . $row['id'] . ")>del</div>";
-        
+          
+       
         
     //$output .= "<table>";
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
        //$output .= "<tr class='row'><td>" . $row['name'] . "</td><td>" . $row['street']  . "</td><td>" . $row['rating']  . "</td><td onClick='delMyRoute(" . $row['id'] . ")'>DEL</td></tr>";
-     
+          $output .= "<div class='name'>" . $row['name'] . "</div>";
+          $output .= "<div class='street'>" . $row['street'] . "</div>";
+          $output .= "<div class='rating'>" . $row['rating'] . "</div>";
+          $output .= "<div class='del' onClick='delMyRoute(" . $row['id'] . ")>del</div>";
     }
     //$output .= "</table>";
+      $output .= "</div>";
+      $output .= "</div">;
     /* free result set */
     $result->free();
 }
